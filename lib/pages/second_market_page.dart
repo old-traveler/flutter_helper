4bc0b9376ac27821a7e4f7925cdc1fc7cea1959b
贺宇成 <heyucheng001@ke.com>
2019-09-29 19:20:36 +0800

    [Change]:[二手市场页面继承BaseRefreshPage]

--- lib/pages/second_market_page.dart
+++ lib/pages/second_market_page.dart
@@ -1,15 +1,12 @@
-import 'dart:convert';
-import 'dart:io';
 import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_easyrefresh/easy_refresh.dart';
-import 'package:flutter_easyrefresh/phoenix_header.dart';
 import 'package:flutter_helper/api/net_api.dart';
+import 'package:flutter_helper/base/base_page_state.dart';
+import 'package:flutter_helper/base/base_result_model.dart';
 import 'package:flutter_helper/entity/second_goods_entity.dart';
 import 'package:flutter_helper/http/http_manager.dart';
 import 'package:flutter_helper/res/strings.dart';
-import 'package:flutter_helper/utils/toast_util.dart';
 
 class SecondMarketPage extends StatefulWidget {
   @override
@@ -18,85 +15,8 @@ class SecondMarketPage extends StatefulWidget {
   }
 }
 
-class _SecondMarketPageState extends State<SecondMarketPage> with AutomaticKeepAliveClientMixin{
-  EasyRefreshController _controller;
-  List<SecondGoodsGood> _secondGoods;
-  int _page = 1;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = EasyRefreshController();
-    _onRefresh();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _controller.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    super.build(context);
-    return EasyRefresh.custom(
-      controller: _controller,
-      header: PhoenixHeader(),
-      footer: ClassicalFooter(),
-      enableControlFinishRefresh: true,
-      enableControlFinishLoad: true,
-      onRefresh: _onRefresh,
-      onLoad: _fetchSecondGoods,
-      slivers: <Widget>[
-        SliverGrid(
-          delegate:
-              // ignore: missing_return
-              SliverChildBuilderDelegate((BuildContext context, int index) {
-            return _getItemWidget(context, index);
-          }, childCount: _secondGoods == null ? 0 : _secondGoods.length),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 5,
-            crossAxisSpacing: 5,
-            childAspectRatio: 0.77,
-          ),
-        )
-      ],
-    );
-  }
-
-  Future _onRefresh() async {
-    _page = 1;
-    _fetchSecondGoods();
-  }
-
-  Future _fetchSecondGoods() async {
-    var response = await HttpManager.getInstance()
-        .get(NetApi.SECOND_GOODS_URL + "$_page/0");
-    if (response == null) {
-      ToastUtil.show(context: context, msg: "网络不给力");
-      _controller.finishRefresh(success: false);
-      _controller.finishLoad(success: false);
-      return;
-    }
-    SecondGoodsEntity entity =
-        SecondGoodsEntity.fromJson(json.decode(response.toString()));
-    if (entity.code == HttpStatus.ok) {
-      setState(() {
-        if (_page == 1) {
-          _secondGoods = List();
-          _controller.finishRefresh(
-              success: true, noMore: entity.currentPage >= entity.pageination);
-        } else {
-          _controller.finishLoad(
-              success: true, noMore: entity.currentPage >= entity.pageination);
-        }
-        _secondGoods.addAll(entity.goods);
-        _page++;
-      });
-    }
-  }
-
+class _SecondMarketPageState
+    extends BaseRefreshState<SecondMarketPage, SecondGoodsGood> {
   Widget _getItemWidget(BuildContext context, int index) {
     double width = (MediaQuery.of(context).size.width - 5) / 2;
     return Card(
@@ -107,7 +27,7 @@ class _SecondMarketPageState extends State<SecondMarketPage> with AutomaticKeepA
       child: Column(
         children: <Widget>[
           Image.network(
-            YStrings.baseImageUrl + _secondGoods[index].image,
+            YStrings.baseImageUrl + getItemData(index).image,
             fit: BoxFit.cover,
             height: width,
             width: width,
@@ -129,7 +49,7 @@ class _SecondMarketPageState extends State<SecondMarketPage> with AutomaticKeepA
           left: 5,
           top: 5,
           child: Text(
-            _secondGoods[index].tit,
+            getItemData(index).tit,
             style: TextStyle(
                 fontSize: 14, color: Colors.black, fontFamily: 'mononoki'),
             maxLines: 2,
@@ -140,7 +60,7 @@ class _SecondMarketPageState extends State<SecondMarketPage> with AutomaticKeepA
           left: 5,
           top: 30,
           child: Text(
-            "¥ " + _secondGoods[index].prize,
+            "¥ " + getItemData(index).prize,
             style: TextStyle(fontSize: 15, color: Colors.redAccent),
             maxLines: 1,
             textAlign: TextAlign.left,
@@ -150,7 +70,7 @@ class _SecondMarketPageState extends State<SecondMarketPage> with AutomaticKeepA
           right: 5,
           top: 30,
           child: Text(
-            _secondGoods[index].createdOn,
+            getItemData(index).createdOn,
             style: TextStyle(fontSize: 10, color: Colors.grey),
             maxLines: 1,
           ),
@@ -161,4 +81,37 @@ class _SecondMarketPageState extends State<SecondMarketPage> with AutomaticKeepA
 
   @override
   bool get wantKeepAlive => true;
+
+  @override
+  getResponse(int _page) {
+    return HttpManager.getInstance().get(NetApi.SECOND_GOODS_URL + "$_page/0");
+  }
+
+  @override
+  List<Widget> getSlivers() {
+    return <Widget>[
+      SliverGrid(
+        delegate:
+            // ignore: missing_return
+            SliverChildBuilderDelegate((BuildContext context, int index) {
+          return _getItemWidget(context, index);
+        }, childCount: getItemCount()),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 5,
+          crossAxisSpacing: 5,
+          childAspectRatio: 0.77,
+        ),
+      )
+    ];
+  }
+
+  @override
+  BasePageResult<SecondGoodsGood> jsonToResult(json) {
+    SecondGoodsEntity entity = SecondGoodsEntity.fromJson(json);
+    return entity == null
+        ? null
+        : BasePageResult(entity.code, null, entity.goods,
+            entity.currentPage >= entity.pageination);
+  }
 }
